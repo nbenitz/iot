9a1e1b62291f822be54438d73323685e6904bccd
Nestor Benitez <n.benitez.a@hotmail.com>
2020-10-17 20:08:36 -0300

    heroku

---
+++ dispositivo/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'dispositivo.apps.DispositivoConfig'
--- dispositivo/apps.py
+++ dispositivo/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
 
-
 class DispositivoConfig(AppConfig):
     name = 'dispositivo'
+    def ready(self):
+        from .mqtt_to_database import mqtt_loop
+        print("startup")
+        # mqtt_loop() # startup code here
