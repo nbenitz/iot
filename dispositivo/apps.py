6f06681d77725bfb1d8355fa99aa9a9cb598bf8f
Nestor Benitez <n.benitez.a@hotmail.com>
2020-10-17 20:36:06 -0300

    heroku

--- dispositivo/apps.py
+++ dispositivo/apps.py
@@ -4,4 +4,7 @@ class DispositivoConfig(AppConfig):
     name = 'dispositivo'
     def ready(self):
         from .mqtt_to_database import mqtt_loop
-        mqtt_loop() # startup code here
+        from project.config import mqtt_run
+        if not mqtt_run:
+            mqtt_loop() # startup code here
+            mqtt_run = True
