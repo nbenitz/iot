4929373272d3f7ad86b591f70e6eb6ec97696a64
Nestor Benitez <n.benitez.a@hotmail.com>
2020-10-17 21:43:31 -0300

    heroku

--- dispositivo/apps.py
+++ dispositivo/apps.py
@@ -1,18 +1,18 @@
 from django.apps import AppConfig
-from django.utils import timezone
+# rom django.utils import timezone
 
 class DispositivoConfig(AppConfig):
     name = 'dispositivo'
     def ready(self):
         from .mqtt_to_database import mqtt_loop
-        from .models import Logs
-        event = 'MQTT to Database connected OK. Returned code = 0'
-        last_pub = Logs.objects.filter(evento=event).last()
-        if last_pub:          
-            seconds_diff = (timezone.now() - last_pub.fecha).total_seconds()
-            if seconds_diff > 120:
-                mqtt_loop()
-        else:
-            mqtt_loop()
+        # from .models import Logs
+        # event = 'MQTT to Database connected OK. Returned code = 0'
+        # last_pub = Logs.objects.filter(evento=event).last()
+        # if last_pub:          
+        #     seconds_diff = (timezone.now() - last_pub.fecha).total_seconds()
+        #     if seconds_diff > 120:
+        #         mqtt_loop()
+        # else:
+        mqtt_loop()
        
             
