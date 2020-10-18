9dde46ee17812cf945a48e202c957f31ffefd0eb
Nestor Benitez <n.benitez.a@hotmail.com>
2020-10-18 12:56:09 -0300

    heroku

--- project/mqtt_to_pg.py
+++ project/mqtt_to_pg.py
@@ -1,4 +1,4 @@
-from settings import DATABASES
+from .settings import DATABASES
 import psycopg2
 import paho.mqtt.client as mqtt
 import datetime
@@ -7,7 +7,6 @@ from functools import reduce
 import operator
 import random
 import string
-from django.utils import timezone
 
 broker_address = "broker.mqtt-dashboard.com"
 broker_port = 1883
@@ -38,19 +37,17 @@ def registrar_sensor(id_sensor, msg, retain):
     try:
         cursor.execute(sql, (id_sensor, msg, now.strftime("%Y-%m-%d %H:%M:%S"), bool(retain)))
         db.commit()
-        print("\nInsert Ok\n")
     except:
         db.rollback()
         print("\nerror :(\n")
 
     db.close
     
-def registrar_feedback_mysql(id_actuador, msg):
-    now = datetime.datetime.now()
-    sql = "INSERT INTO `publicacion_actuador` (`id_actuador_fk`, `valor`, `fecha`) VALUES ({0}, {1}, '{2}')".format(id_actuador, msg, now.strftime("%Y-%m-%d %H:%M:%S"))  
-    #print(sql)
+def registrar_feedback(id_actuador, msg, retain):
+    now = datetime.datetime.now().astimezone(pytz.timezone('UTC'))
+    sql = "INSERT INTO publicacion_actuador (id_actuador_fk, valor, fecha, retain) VALUES(%s, %s, %s, %s)"
     try:
-        cursor.execute(sql)
+        cursor.execute(sql, (id_actuador, msg, now.strftime("%Y-%m-%d %H:%M:%S"), bool(retain)))
         db.commit()
     except:
         db.rollback()
@@ -59,19 +56,30 @@ def registrar_feedback_mysql(id_actuador, msg):
     db.close
 
 def registrar_log(log):
-    now = timezone.now()
-    # try:
-    #     pub = Logs(fecha=now, evento=log)
-    #     pub.save()
-    # except:
-    #     print("\nError :(\n")
+    now = datetime.datetime.now().astimezone(pytz.timezone('UTC'))
+    sql = "INSERT INTO logs (fecha, evento) VALUES(%s, %s)"
+    try:
+        cursor.execute(sql, (now.strftime("%Y-%m-%d %H:%M:%S"), log))
+        db.commit()
+    except:
+        db.rollback()
+        print("error")
+        
+    db.close
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-    print("UserData= " + str(userdata))
-    print("flags= " + str(flags))
-    print("")
-    client.subscribe(topic)
+    msg = ""
+    if rc == 0:
+        msg = "MQTT to Database connected OK. Returned code = " + str(rc)
+        print("Connected OK Returned code = " + str(rc))
+        print("UserData= " + str(userdata))
+        print("flags= " + str(flags))
+        print("")
+        client.subscribe(topic)
+    else:
+        msg = "MQTT to Database Bad connection. Returned code = " + str(rc)
+        print("Bad connection Returned code=", rc)
+    registrar_log(msg)
 
 def on_message(client, userdata, message):
     msg = str(message.payload.decode("utf-8"))
@@ -89,17 +97,16 @@ def on_message(client, userdata, message):
     if sub_topic == "sensor":
         registrar_sensor(id_disp, msg, retain)
 
-    # if sub_topic == "feedback":
-    #     registrar_feedback(id_disp, msg, retain)
+    if sub_topic == "feedback":
+        registrar_feedback(id_disp, msg, retain)
 
-    # if sub_topic == "status":
-    #     registrar_status(id_disp, msg, retain)
+    if sub_topic == "status":
+        registrar_status(id_disp, msg, retain)
 
 
 def on_disconnect(client, userdata, rc):
     registrar_log("Disconnecting. Reason:  " + str(rc))
 
-#@background(schedule=5)
 def mqtt_loop():
     print("loop")
     client = mqtt.Client('Cliente1', userdata="UsuarioServer") 
